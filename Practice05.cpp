b01bf3e6b7a544ff461ce9428434b90088dee094
Moinkhan21 <moinmykhan07@gmail.com>
2026-03-31 19:58:19 +0530

    isPrime number on not

---
+++ Practice05.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool findingNumberisPrime(int n) {
+
+    if( n <= 1) return false;
+    
+    for(int i = 2; i < n; i++) {
+        if((n % i) == 0) {
+            return false;
+        }
+    }return true;
+}
+
+int main() {
+    cout << "Enter a number: ";
+    int n;
+    cin >> n;
+
+    int isNumberPrime = findingNumberisPrime(n);
+
+    if(isNumberPrime){
+        cout << n << " Number is prime";
+    }
+    else {
+        cout << n << " Number is not prime";
+    }
+}
